a24edca4a8d8b492a63309e128cea9b14e8912d6
Jason K <jasonk54@gmail.com>
2013-05-10 14:24:37 -0700

    Geolocation, geocoding, markers finished

--- maps.js
+++ maps.js
@@ -1,15 +1,27 @@
 
 // Displays and locates the user's location
-
+var geocoder;
 var map;
+var lat = 37.783;
+var lng = -122.409;
+var markerArray = [];
 
-var geoLocation = function() {
+// Locates the user on the map
+function initialize() {
+  // Translate address to Lat and Lng
+  geocoder = new google.maps.Geocoder();
+
   var mapOptions = {
     zoom: 12,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   map = new google.maps.Map(document.getElementById('map-canvas'),
     mapOptions);
+
+  // Adds marker on click
+  google.maps.event.addListener(map, 'click', function(event) {
+    placeMarker(event.latLng);
+  });
 
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
@@ -25,12 +37,12 @@ var geoLocation = function() {
       handleNoGeolocation(true);
     });
   } else {
-    // If GeoLocation is not supported, calls handleNoGeoLocation function
+  // If GeoLocation is not supported, calls the handleNoGeoLocation function
     handleNoGeolocation(false);
   }
-}
+};
 
-function handleNoGeolocation(errorFlag) {
+var handleNoGeolocation = function(errorFlag) {
   if (errorFlag) {
     var content = 'Error: The Geolocation service failed.';
   } else {
@@ -40,11 +52,52 @@ function handleNoGeolocation(errorFlag) {
   // If GeoLocation is not possible, defaults to San Francisco area.
   var options = {
     map: map,
-    position: new google.maps.LatLng(37.783, -122.409),
+    position: new google.maps.LatLng(lat, lng),
   };
 
   var infowindow = new google.maps.InfoWindow(options);
   map.setCenter(options.position);
+};
+
+// If clicked, draws marker
+var placeMarker = function(location) {
+  // Clears the marker (does not delete) from the map before placing the new marker
+  clearMarker();
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+  markerArray.push(marker);
+  // map.setCenter(marker.getPosition());
+  console.log('marker location: ' + marker.getPosition());
 }
 
-google.maps.event.addDomListener(window, 'load', geoLocation);
+// Clear markers
+var clearMarker = function() {
+  if (markerArray) {
+    for (var i = 0; i < markerArray.length; i++) {
+      markerArray[i].setMap(null);
+    }
+  markerArray = []
+  };
+};
+
+// Helper function to translate address to LatLng
+// Need to input #address
+// var codeAddress = function() {
+//   var address = '944 market st., san francisco, ca'
+//   // var address = document.getElementById("address").value;
+//   geocoder.geocode( { 'address': address}, function(results, status) {
+//     if (status == google.maps.GeocoderStatus.OK) {
+//       map.setCenter(results[0].geometry.location);
+//       var marker = new google.maps.Marker({
+//           map: map,
+//           position: results[0].geometry.location
+//       });
+//     } else {
+//       alert("Geocode was not successful for the following reason: " + status);
+//     }
+//   });
+// }
+
+google.maps.event.addDomListener(window, 'load', initialize);
